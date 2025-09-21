7ef526d9c152483968d6288a6b448fc4986d8004
Viusan <viusansantos@gmail.com>
2025-09-21 22:06:38 +0200

    adsadkapdad

--- Main.java
+++ Main.java
@@ -7,10 +7,10 @@ public class Main{
     a[j] = tmp;
   }
 
+//Oppgave 1
   static int partition(int[] arr, int low, int high){
     int pivot = arr[high];//Choose the pivot
     int i = low-1;
-
     for(int j = low; j <= high-1; j++){
       //Notice i is set to low -1, so it is "one step behind" the loop
       //The loop checks if current index is smaller then the pivot, if not then next, checks again and if so the previous element must be higher then the current so swap
@@ -19,12 +19,10 @@ public class Main{
         changeIndex(arr, i, j);
       }
     }
-
     //Finally we know which elements are higher then pivot, and swap places by putting it in the middle.
     changeIndex(arr, i+1, high);
     return i+1;
   }
-
   static void quickSort(int[] arr, int low, int high){
     if(low < high){
       int pi = partition(arr, low, high);
@@ -35,6 +33,10 @@ public class Main{
     }
   }
 
+//Oppgave 2  
+  public static int antallUlikeSortert(int[] a){
+    return a;
+  } 
   public static void main(String[] args){
     int[] numbers = {1, 3, 9, 2, 5};
     int n = numbers.length;
